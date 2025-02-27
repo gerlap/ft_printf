2bbd03591125f724d4232f4fad04e8a1b362e618
gerlap <georgiilapshin@gmail.com>
2025-02-27 19:59:05 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: georgiilapshin <georgiilapshin@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 06:39:24 by georgiilaps       #+#    #+#             */
+/*   Updated: 2025/02/27 19:49:47 by georgiilaps      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int ft_printf(const char *str, ...);
+int ft_putchar_fd(char c, int fd);
+int ft_putstr(char *str);
+int ft_putnbr_fd(int n);
+int ft_putunnbr(unsigned int n);
+int ft_puthex(unsigned int n, char x);
+int ft_put_p(void *ptr);
+
+#endif
